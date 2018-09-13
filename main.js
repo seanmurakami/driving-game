096a100bf24336979d15020112a51b70dd0f2b7d
Sean Murakami <murakami.sean@yahoo.com>
2018-09-13 13:54:39 -0700

    #3 modified the move method by adding the style property of the image element to change its pixel-wise top and left position accrording to the cars new location

--- main.js
+++ main.js
@@ -13,15 +13,19 @@ class Car {
   move() {
     if (this.direction === 'north') {
       this.location[1] += this.speed
+      this.$img.setAttribute('style', 'position: absolute; top: ' + Math.abs(this.location[1])*(4*this.speed) + 'em; left: ' + Math.abs(this.location[0])*(4*this.speed) + 'em')
     }
     if (this.direction === 'south') {
       this.location[1] -= this.speed
+      this.$img.setAttribute('style', 'position: absolute; top: ' + Math.abs(this.location[1])*(4*this.speed) + 'em; left: ' + Math.abs(this.location[0])*(4*this.speed) + 'em')
     }
     if (this.direction === 'east') {
       this.location[0] += this.speed
+      this.$img.setAttribute('style', 'position: absolute; top: ' + Math.abs(this.location[1])*(4*this.speed) + 'em; left: ' + Math.abs(this.location[0])*(4*this.speed) + 'em')
     }
     if (this.direction === 'west') {
       this.location[0] -= this.speed
+      this.$img.setAttribute('style', 'position: absolute; top: ' + Math.abs(this.location[1])*(4*this.speed) + 'em; left: ' + Math.abs(this.location[0])*(4*this.speed) + 'em')
     }
   }
 }
@@ -35,14 +39,18 @@ var car1 = new Car($image, 1, 'north', [0, 0])
 document.addEventListener('keydown', function (e) {
   if (e.keyCode === 38) {
     car1.turn('north')
+    car1.move()
   }
   if (e.keyCode === 40) {
     car1.turn('south')
+    car1.move()
   }
   if (e.keyCode === 37) {
     car1.turn('west')
+    car1.move()
   }
   if (e.keyCode === 39) {
     car1.turn('east')
+    car1.move()
   }
 })
